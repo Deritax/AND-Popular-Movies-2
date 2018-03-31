9f40b68667bb1f6c90bb73c7004e95d2f6619e23
Deritax <kaeltas90@gmail.com>
2018-03-31 15:52:42 +0200

    part 2

--- app/src/main/java/com/itarusoft/movies/ListViewNoScroll.java
+++ app/src/main/java/com/itarusoft/movies/ListViewNoScroll.java
@@ -6,19 +6,23 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 /**
-* https://www.journaldev.com/10444/android-custom-listview-non-scrollable
+ *  I take this code to solve the scrolling problem on the listview
+ * source: https://www.journaldev.com/10444/android-custom-listview-non-scrollable
 * */
 public class ListViewNoScroll extends ListView{
 
         public ListViewNoScroll(Context context) {
             super(context);
         }
+
         public ListViewNoScroll(Context context, AttributeSet attrs) {
             super(context, attrs);
         }
+
         public ListViewNoScroll(Context context, AttributeSet attrs, int defStyle) {
             super(context, attrs, defStyle);
         }
+
         @Override
         public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             int heightMeasureSpec_custom = MeasureSpec.makeMeasureSpec(
